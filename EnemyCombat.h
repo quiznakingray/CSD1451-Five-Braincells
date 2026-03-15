860d8b8f2b1fd5d910312ca09b5b564ab2749337
222839K <222839K@mymail.nyp.edu.sg>
2026-03-15 17:06:20 +0800

    Revert "Merge branch 'main' into Tilemap-Xin-Yi"
    This reverts commit 7314c114d73d585d9aa80357b70274fce7d32177, reversing
    changes made to 37e9fe4d873c8da8b73e3995866470f53e6ebe3a.

--- EnemyCombat.h
+++ EnemyCombat.h
@@ -1,22 +1,11 @@
 #ifndef ENEMY_COMBAT_H
 #define ENEMY_COMBAT_H
 
-#include "AEEngine.h"
+#include "EnemyBase.h"
 
-struct EnemyStats {
-    s32 health;
-    s32 maxHealth;
-    s32 attack;
-    f32 speed;
-    const char* name;
-};
+struct Player;
 
-struct PlayerStats;
-
-void EnemyAttackPlayer(EnemyStats& enemyStats, Player& player);
-bool IsEnemyDead(const EnemyStats& enemyStats);
-void TakeDamage(EnemyStats& enemyStats, s32 damage);
-void InstantKill(EnemyStats& enemyStats);
-void TurnOrder(EnemyStats& enemyStats, PlayerStats& playerStats, s32 playerDmg);
+void EnemyAttackPlayer(EnemyBase& enemy, Player& player);
+bool IsEnemyDead(const EnemyBase& enemy);
 
 #endif
