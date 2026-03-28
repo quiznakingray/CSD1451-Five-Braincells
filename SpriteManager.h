f6298a1d02881c3a5005365da10debb9cf16a458
rachel <109144971+quiznakingray@users.noreply.github.com>
2026-03-28 18:43:15 +0800

    added texture change when switching textures

--- SpriteManager.h
+++ SpriteManager.h
@@ -42,6 +42,7 @@ struct Sprite : ComponentBase{
 	AEGfxVertexList* mesh = nullptr;
 	AEGfxTexture* texture = nullptr;
 	std::string textureFileName{};
+	std::string prevTextureFileName{};
 	SPRITE_SHAPE spriteShape = SPRITE_SHAPE::SHAPE_RECT;
 	//std::array <f64, COLORSIZE > color{ };
 	struct SpriteSheet {
