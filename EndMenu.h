77fb097689c0544ac02cb53614f2b74676872a00
Spooderman-droid <zijun17azj@gmail.com>
2026-04-01 10:18:06 +0800

    Update EndMenu.h

--- EndMenu.h
+++ EndMenu.h
@@ -1,19 +1,22 @@
 #ifndef END_MENU_H
 #define END_MENU_H
 
+#include "SingletonTemplate.h"
+
 struct EndMenu {
-    static bool isActive;    // Is the pop-up currently showing?
-    static bool isWin;       // True for "Congrats", False for "You Are Dead"
+    bool isActive = true;    // Is the pop-up currently showing?
+    bool isWin = false;       // True for "Congrats", False for "You Are Dead"
 
     // Stats Placeholders
-    static int score;
-    static float timeTaken;
-    static int deathCounter;
+    int score = 0;
+    float timeTaken = 0.0f;
+    int deathCounter = 0;
 
-    static void Init();
-    static void Update();
-    static void Render();
-    static void Free();
+    void Init();
+    void Update();
+    void Render();
+    void Free();
+
 };
 
 #endif
